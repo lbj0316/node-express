0d6b2f36d525e0d483c5df0fd913151c771578ab
lbj0316 <128190545+lbj0316@users.noreply.github.com>
2023-03-18 07:17:05 +0900

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res, next) {
   res.render('index.html');
 });
 
+/**
+ * @path http://localhost:3000/ 경로
+ */
+router.get("/timer/", (req, res) => {
+    res.send("Hello World");
+});
+
 module.exports = router;
