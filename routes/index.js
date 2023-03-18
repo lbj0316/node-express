dd96653f3099865b95db1376ccd1e8660d5eeb7a
lbj0316 <128190545+lbj0316@users.noreply.github.com>
2023-03-18 12:39:43 +0900

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,12 @@
 var express = require('express');
 var router = express.Router();
-const dbconfig   = require('./config/database.js');
-const connection = mysql.createConnection(dbconfig);
+// const dbconfig   = require('./config/database.js');
+const connection = mysql.createConnection({
+  host     : 'svc.sel3.cloudtype.app:31727',
+  user     : 'holdem',
+  password : 'Qswer!23',
+  database : 'timer'
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -16,11 +21,13 @@ router.get("/timer/", (req, res) => {
 });
 
 router.get('/timerlist', (req, res) => {
+  connection.connect();
   connection.query('SELECT * from timer_list', (error, rows) => {
     if (error) throw error;
     console.log('timer list is: ', rows);
     res.send(rows);
   });
+  connection.end();
 });
 
 module.exports = router;
